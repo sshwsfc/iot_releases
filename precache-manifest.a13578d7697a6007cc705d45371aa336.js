12d1cabfc8790daf88bfa5f6710303c9862d310c
tianmiao <23025006@qq.com>
2021-02-04 16:09:01 +0800

    Updates

---
+++ precache-manifest.a13578d7697a6007cc705d45371aa336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94c24616aaf07572a687",
+    "url": "./static/css/main.1e370567.chunk.css"
+  },
+  {
+    "revision": "94c24616aaf07572a687",
+    "url": "./static/js/main.e2920cf9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "004656ca32f51bf71d27",
+    "url": "./static/css/2.2ecc2e6d.chunk.css"
+  },
+  {
+    "revision": "004656ca32f51bf71d27",
+    "url": "./static/js/2.35af6c75.chunk.js"
+  },
+  {
+    "revision": "70b83dfe9410d19ed4fd2d78842d5803",
+    "url": "./static/media/noData.70b83dfe.png"
+  },
+  {
+    "revision": "e7cb887c87f8023dffda07e7e6d9d0d8",
+    "url": "./index.html"
+  }
+];
